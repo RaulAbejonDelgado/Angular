a8f6432d4db6a62bbc75be15b1639d515abf58b4
RaulAbejonDelgado <raul.abejon.delgado@gmail.com>
2018-05-16 18:15:25 +0200

    Integracion del body de la pagina principal

--- html_template_to_web_app/src/app/app.module.ts
+++ html_template_to_web_app/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { PortafolioComponent } from './components/portafolio/portafolio.component';
 //aqui debemos importar los componentes customizados
 //ngmodule decorador que nos permite dentro del modulo principal(app) definir ciertas configuraciones
 @NgModule({
@@ -12,7 +13,8 @@ import { FooterComponent } from './components/footer/footer.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PortafolioComponent
   ],
   //modulos internos de angular o modulos nuestros
   imports: [
